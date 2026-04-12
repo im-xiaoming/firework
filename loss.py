3a8e26a8a29f1988e577916435b24d1a6e28987a
xiao mingg <minhnguyen47431@gmail.com>
2026-04-12 14:52:54 +0700

    fix

--- loss.py
+++ loss.py
@@ -4,12 +4,16 @@ from .function import softmax
 
 class CrossEntropyLoss(nn.Module):
     def __init__(self):
-        super(CrossEntropyLoss, self).__init__()
+        super().__init__()
     
     def forward(self, outputs, targets):
-            assert outputs.ndim == 2, "Outputs must be 2-dimensions"
-            assert targets.ndim == 1, "Targets must be 1-dimensions"
-            
-            logits = softmax(outputs) 
-            filters = logits[torch.arange(targets.size(0)), targets]
-            return torch.mean(-torch.log(filters))
+        x = outputs - torch.max(outputs, dim=-1, keepdim=True)[0]
+        exp_x = torch.exp(x)
+        probs = exp_x / torch.sum(exp_x, dim=-1, keepdim=True)
+
+        filters = probs[torch.arange(targets.size(0)), targets]
+
+        eps = 1e-9
+        loss = -torch.log(filters + eps)
+
+        return loss.mean()
